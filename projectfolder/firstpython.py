658686098414c6e9e5f7932e5c0b8a284626955c
vasundhra18 <vasundharasingh177@gmail.com>
2026-05-12 23:09:20 +0530

    pushing python project

---
+++ projectfolder/firstpython.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+# Define two variables
+a = 10
+b = 5
+
+# Sum
+sum_result = a + b
+
+# Subtraction
+sub_result = a - b
+
+# Print results
+print("Sum:", sum_result)
+print("Subtraction:", sub_result)
