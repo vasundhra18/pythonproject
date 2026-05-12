925bae94635c110e31362138daf2b47b829fa7b4
vasundhra18 <vasundharasingh177@gmail.com>
2026-05-12 23:34:57 +0530

    pushing php project repo

---
+++ projectfolder/firstphpfile.php
@@ -0,0 +1,15 @@
+<?php
+// Define two variables
+$a = 20;
+$b = 4;
+
+// Multiplication
+$multiplication = $a * $b;
+
+// Division
+$division = $a / $b;
+
+// Print results
+echo "Multiplication: " . $multiplication . "\n";
+echo "Division: " . $division;
+?>
